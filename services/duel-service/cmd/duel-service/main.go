8bbb2fb534c3b919d46032fb865129e7e97247a1
Rafael Santos <15151000+blackmagicbox@users.noreply.github.com>
2026-08-10 23:34:44 +0200

    Add graceful shutdown to duel service

--- services/duel-service/cmd/duel-service/main.go
+++ services/duel-service/cmd/duel-service/main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log/slog"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func main() {
@@ -16,14 +20,39 @@ func main() {
 		port = "50051"
 	}
 
-	http.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(200)
 		w.Write([]byte("OK\n"))
 	})
 
-	slog.Info("Starting duel-service", "port", port)
-	if err := http.ListenAndServe(fmt.Sprintf(":%s", port), nil); err != nil {
-		slog.Error("Server failed to start", "error", err)
+	server := &http.Server{
+		Addr:    fmt.Sprintf(":%s", port),
+		Handler: mux,
+	}
+
+	go func() {
+		slog.Info("Starting duel-service", "port", port)
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			slog.Error("Server failed to start", "error", err)
+			os.Exit(1)
+		}
+	}()
+
+	<-ctx.Done()
+	slog.Info("Shutdown signal received")
+
+	shutDownContext, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(shutDownContext); err != nil {
+		slog.Error("Gracefull shutdown failed", "error", err)
 		os.Exit(1)
 	}
+
+	slog.Info("Server stopped gracefully")
+
 }
